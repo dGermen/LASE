4731112dbd7ec8ae5f22c5070a79f769d34669c6
ardan-yilmaz <yilmazardan@gmail.com>
2023-05-19 00:07:30 +0300

    FileManager class

---
+++ file_manager.py
@@ -0,0 +1,34 @@
+class FileManager:
+
+    def __init__(self, folder_path, pdf_processor, embedder, whoosh, config):
+        self.PDF_manager = pdf_processor
+        self.whoosh = whoosh
+        self.embedder = embedder
+        self.folder_path = folder_path
+        self.config = config
+    
+
+    def file_processor(self, file_name):
+        # 1. whoosh processing
+        self.whoosh.add_index(file_name)
+
+        # 2. pdf extractor 
+        # metadata = {title: ,
+        #             abstract: ,
+        #             dir: }
+
+        metadata, text = self.PDF_manager.process_pdf(file_name)   
+
+        # 3. get embeddings
+        text_embed = self.embedder(text)
+
+        # 4. add id and embeddings fields to dict
+        metadata["embedding"] = text_embed
+        config += 1
+        metadata["id"] = config
+
+        # 5. return metadata & embeds
+        return metadata, text_embed
+    
+
+
