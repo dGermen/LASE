29cf98bde144a413e453bd0675fb53e8fb07a8a2
ardan-yilmaz <yilmazardan@gmail.com>
2023-05-19 00:31:57 +0300

    added id to schema and search returns the top k ids

--- whoosh_manager.py
+++ whoosh_manager.py
@@ -1,63 +1,45 @@
 import os
 from whoosh import index
-from whoosh.fields import Schema, TEXT
+from whoosh.fields import Schema, TEXT, NUMERIC
 from whoosh.qparser import QueryParser
 from whoosh.index import create_in, open_dir
 from PyPDF2 import PdfReader
+import numpy as np
 
 
 class WhooshIRProcessor:
     def __init__(self, pdf_dir):
         self.index_dir = "index"
         self.pdf_dir = pdf_dir
-        self.schema = Schema(title=TEXT(stored=True), content=TEXT(stored=True))
+        self.schema = Schema(id=NUMERIC(stored=True, unique=True), title=TEXT(stored=True), content=TEXT(stored=True))
 
-    def create_index(self):
-        if not os.path.exists(self.index_dir):
-            os.mkdir(self.index_dir)
-
-        ix = create_in(self.index_dir, self.schema)
-        writer = ix.writer()
-
-        for root, _, files in os.walk(self.pdf_dir):
-            for file in files:
-                if file.endswith(".pdf"):
-                    pdf_path = os.path.join(root, file)
-                    self.index_pdf(writer, pdf_path)
-
-        writer.commit()
     
-    def add_index(self, f_name):
-        pdf_path = os.path.join(self.pdf_dir, f_name)
+    def add_index(self, pdf_path, doc_id):
         if not os.path.exists(self.index_dir):
             os.mkdir(self.index_dir)
 
         ix = open_dir(self.index_dir)
         writer = ix.writer()
-        self.index_pdf(writer, pdf_path)
+        self.index_pdf(writer, pdf_path, doc_id)
         writer.commit()
 
-    def index_pdf(self, writer, pdf_path):
+    def index_pdf(self, writer, pdf_path, doc_id):
         with open(pdf_path, 'rb') as file:
             pdf = PdfReader(file)
             text = ''
             for page in pdf.pages:
                 text += page.extract_text()
 
-            writer.add_document(title=os.path.basename(pdf_path), content=text)
+            writer.add_document(id=str(doc_id), title=os.path.basename(pdf_path), content=text)
 
-    def search(self, query_str):
+    def search(self, query_str, k=10):
         ix = open_dir(self.index_dir)
         searcher = ix.searcher()
         query = QueryParser("content", ix.schema).parse(query_str)
-        results = searcher.search(query)
-
-        for hit in results:
-            print(f"Title: {hit['title']}")
-            print(f"Score: {hit.score:.4f}")
-            # print(f"Content: {hit['content']}\n")
-
+        results = searcher.search(query, limit=k)
+        top_k_ids = np.array([hit['id'] for hit in results])
         searcher.close()
+        return top_k_ids.tolist()        
 
 
 """
