1cba5a36a2bc21a81d071a56a00062171036b1b0
ardan-yilmaz <yilmazardan@gmail.com>
2023-06-01 22:42:53 +0300

    basic GUI

--- GUI.py
+++ GUI.py
@@ -1,96 +1,103 @@
-import wx
+from PyQt5 import QtCore, QtGui, QtWidgets
 from manager import Manager
 import os
 import subprocess
 
-class Mywin(wx.Frame): 
-    def __init__(self, parent, title): 
-        super(Mywin, self).__init__(parent, title = title, size = (500,500)) 
+class MyWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
 
         self.manager = Manager()
 
-        panel = wx.Panel(self) 
-        box = wx.BoxSizer(wx.VERTICAL)
+        self.setWindowTitle('IR System')
 
-        self.text = wx.TextCtrl(panel, size = (400,30)) 
-        box.Add(self.text,0,flag = wx.EXPAND|wx.ALL,border = 5) 
+        central_widget = QtWidgets.QWidget(self)
+        self.setCentralWidget(central_widget)
 
-        button = wx.Button(panel, label = "Search") 
-        box.Add(button,0,flag = wx.EXPAND|wx.ALL,border = 5) 
-        button.Bind(wx.EVT_BUTTON, self.OnSearch)
+        layout = QtWidgets.QVBoxLayout(central_widget)
+
+        self.text = QtWidgets.QLineEdit()
+        layout.addWidget(self.text)
+
+        button = QtWidgets.QPushButton('Search')
+        layout.addWidget(button)
+        button.clicked.connect(self.OnSearch)
+
+        results_layout = QtWidgets.QHBoxLayout()
+        layout.addLayout(results_layout)
 
         # IR results title
-        ir_title = wx.StaticText(panel, label="Whoosh Results")
-        box.Add(ir_title, 0, flag=wx.ALIGN_CENTER_HORIZONTAL|wx.TOP, border=10)
-        self.ir_list_ctrl = wx.ListCtrl(panel, style=wx.LC_REPORT)
-        self.ir_list_ctrl.InsertColumn(0, 'Title')
-        self.ir_list_ctrl.InsertColumn(1, 'Abstract', width=200)
-        self.ir_list_ctrl.InsertColumn(2, 'Path')
-        self.ir_list_ctrl.Bind(wx.EVT_LIST_ITEM_ACTIVATED, self.OnItemActivated)
-        self.ir_list_ctrl.SetColumnWidth(1, wx.LIST_AUTOSIZE_USEHEADER)
-        box.Add(self.ir_list_ctrl, 1, flag=wx.EXPAND)
+        ir_title = QtWidgets.QLabel('Whoosh Results')
+        ir_layout = QtWidgets.QVBoxLayout()
+        results_layout.addLayout(ir_layout)
+        ir_layout.addWidget(ir_title)
+        self.ir_model = QtGui.QStandardItemModel(self)
+        self.ir_model.setHorizontalHeaderLabels(['Title', 'Abstract', 'Path'])
+        self.ir_view = QtWidgets.QTableView()
+        self.ir_view.setModel(self.ir_model)
+        self.ir_view.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
+        self.ir_view.horizontalHeader().setHighlightSections(False)
+        self.ir_view.verticalHeader().setDefaultSectionSize(3 * self.ir_view.verticalHeader().defaultSectionSize())
+        self.ir_view.verticalHeader().setVisible(False)
+        self.ir_view.setShowGrid(True)
+        self.ir_view.setGridStyle(QtCore.Qt.SolidLine)
+        self.ir_view.setStyleSheet("QTableView { gridline-color: black } ")
+        ir_layout.addWidget(self.ir_view)
 
-        ir_title = wx.StaticText(panel, label="Embedding Results")
-        box.Add(ir_title, 0, flag=wx.ALIGN_CENTER_HORIZONTAL|wx.TOP, border=10)
-        self.embed_list_ctrl = wx.ListCtrl(panel, style=wx.LC_REPORT)
-        self.embed_list_ctrl.InsertColumn(0, 'Title')
-        self.embed_list_ctrl.InsertColumn(1, 'Abstract', width=200)
-        self.embed_list_ctrl.InsertColumn(2, 'Path')
-        self.embed_list_ctrl.Bind(wx.EVT_LIST_ITEM_ACTIVATED, self.OnItemActivated)
-        self.ir_list_ctrl.SetColumnWidth(1, wx.LIST_AUTOSIZE_USEHEADER)
-        box.Add(self.embed_list_ctrl, 1, flag=wx.EXPAND)
-        
-        panel.SetSizerAndFit(box)  
+        # Embedding results title
+        embed_title = QtWidgets.QLabel('Embedding Results')
+        embed_layout = QtWidgets.QVBoxLayout()
+        results_layout.addLayout(embed_layout)
+        embed_layout.addWidget(embed_title)
+        self.embed_model = QtGui.QStandardItemModel(self)
+        self.embed_model.setHorizontalHeaderLabels(['Title', 'Abstract', 'Path'])
+        self.embed_view = QtWidgets.QTableView()
+        self.embed_view.setModel(self.embed_model)
+        self.embed_view.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
+        self.embed_view.horizontalHeader().setHighlightSections(False)
+        self.embed_view.verticalHeader().setDefaultSectionSize(3 * self.embed_view.verticalHeader().defaultSectionSize())
+        self.embed_view.verticalHeader().setVisible(False)
+        self.embed_view.setShowGrid(True)
+        self.embed_view.setGridStyle(QtCore.Qt.SolidLine)
+        self.embed_view.setStyleSheet("QTableView { gridline-color: black } ")
+        embed_layout.addWidget(self.embed_view)
 
-        self.Centre() 
-        self.Show(True) 
+        self.ir_view.doubleClicked.connect(self.OnItemActivated)
+        self.embed_view.doubleClicked.connect(self.OnItemActivated)
 
-
-    def OnSearch(self, event):
-        query = self.text.GetValue()
-
-        # uncomment this
-        #results = self.manager.query(query)
-
+    def OnSearch(self):
+        query = self.text.text()
+        # results = self.manager.query(query)
         results = [[1,2], [3,4] ]
+        self.populate_table(self.ir_model, results[0])
+        self.populate_table(self.embed_model, results[1])
 
-        self.ir_list_ctrl.DeleteAllItems()  # clear old results
-        self.embed_list_ctrl.DeleteAllItems()  # clear old results
 
-
-        # process the classical retrieval results from Manager.vis_data   
-        for id in results[0]:  
-            # get the item with id from self.manager.vis_data
+    def populate_table(self, model, ids):
+        model.removeRows(0, model.rowCount())
+        for id in ids:
             paper_dict = self.manager.vis_data[id]
-            index = self.ir_list_ctrl.InsertItem(self.ir_list_ctrl.GetItemCount(), paper_dict["title"])
-            self.ir_list_ctrl.SetItem(index, 1, paper_dict["abstract"])
-            self.ir_list_ctrl.SetItem(index, 2, paper_dict["dir"])
+            items = [QtGui.QStandardItem(paper_dict['title']), QtGui.QStandardItem(paper_dict['abstract']), QtGui.QStandardItem(paper_dict['dir'])]
+            model.appendRow(items)
 
-        # process the embedding results from Manager.embed_data
-        for id in results[1]:
-            # get the item with id from self.manager.vis_data
-            paper_dict = self.manager.vis_data[id]
-            index = self.embed_list_ctrl.InsertItem(self.embed_list_ctrl.GetItemCount(), paper_dict["title"])
-            self.embed_list_ctrl.SetItem(index, 1, paper_dict["abstract"])
-            self.embed_list_ctrl.SetItem(index, 2, paper_dict["dir"])
+    def OnItemActivated(self, index):
+        if index.column() == 1:  # Show the Abstract text in a scrollable MessageBox when double-clicked
+            abstract = index.sibling(index.row(), 1).data()
+            msg = QtWidgets.QMessageBox()
+            msg.setWindowTitle("Abstract")
+            msg.setText(abstract)
+            msg.setStandardButtons(QtWidgets.QMessageBox.Ok)
+            msg.exec_()
+        elif index.column() == 2:  # Open the paper only when 'Path' is double clicked
+            path = index.sibling(index.row(), 2).data()
+            paper = path.split("/")[-1]
+            path1 = os.path.join(os.getcwd(), "papers")
+            path = os.path.join(path1, paper)
 
-    def OnItemActivated(self, event):
-        print("clicked")
-        item_index = event.GetIndex()  # gets the index of the clicked item
-        path = self.ir_list_ctrl.GetItemText(item_index, 2)  # retrieve text from the third column ('Path')
+            if os.path.isfile(path):
+                subprocess.Popen([path], shell=True)
 
-        paper = path.split("/")[-1]
-
-        path1 = os.path.join(os.getcwd(), "papers")
-        path = os.path.join(path1, paper)
-        print(path)
-        if os.path.isfile(path):
-            print("opening file")
-            os.startfile(path)
-             
-
-
-
-app = wx.App() 
-Mywin(None, 'IR System') 
-app.MainLoop()
+app = QtWidgets.QApplication([])
+window = MyWindow()
+window.show()
+app.exec_()
